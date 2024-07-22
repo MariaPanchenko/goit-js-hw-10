aeb18c50c4bd547ac6af6d242b71fb8906072eb2
MariaPanchenko <marysa2502@gmail.com>
2024-07-22 23:27:32 +0300

    add 1 and 2 tasks

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,55 @@
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
+
+const delayInputEl = document.querySelector('.delay-input');
+const buttonEl = document.querySelector('button');
+const radioInputs = document.getElementsByName('state');
+
+function getPromiseType() {
+  for (let i = 0; i < radioInputs.length; i++) {
+    if (radioInputs[i].checked) return radioInputs[i].value;
+  }
+}
+
+function createPromise(type, delayValue) {
+  const delay = Number(delayValue);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (type === 'fulfilled') {
+        // Fulfill
+        resolve({ type, delay });
+      } else {
+        // Reject
+        reject({ type, delay });
+      }
+    }, delay);
+  });
+}
+
+function onCreateNotification(event) {
+  event.preventDefault();
+  const promiseType = getPromiseType();
+  const delayValue = delayInputEl.value;
+
+  if (promiseType && delayValue) {
+    createPromise(promiseType, delayValue)
+      .then(({ type, delay }) => {
+        iziToast.success({
+          timeout: 3000,
+          position: 'topRight',
+          title: `✅ Fulfilled promise in ${delay}ms`,
+        });
+      })
+      .catch(({ type, delay }) => {
+        iziToast.error({
+          timeout: 3000,
+          position: 'topRight',
+          title: `❌ Rejected promise in ${delay}ms`,
+        });
+      });
+  }
+}
+
+buttonEl.addEventListener('click', onCreateNotification);
