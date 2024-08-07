96fd9c4d5ff830d47fe0638eb202b73b50184409
MariaPanchenko <marysa2502@gmail.com>
2024-08-07 12:41:59 +0300

    fix snackbar

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -3,6 +3,10 @@ import iziToast from 'izitoast';
 // Додатковий імпорт стилів
 import 'izitoast/dist/css/iziToast.min.css';
 
+// отримуємо доступ до форми і вішаємо на неї обробник події
+const form = document.querySelector('.form');
+form.addEventListener('submit', handleSubmit);
+
 const delayInputEl = document.querySelector('.delay-input');
 const buttonEl = document.querySelector('button');
 const radioInputs = document.getElementsByName('state');
@@ -28,7 +32,7 @@ function createPromise(type, delayValue) {
   });
 }
 
-function onCreateNotification(event) {
+function handleSubmit(event) {
   event.preventDefault();
   const promiseType = getPromiseType();
   const delayValue = delayInputEl.value;
@@ -52,4 +56,4 @@ function onCreateNotification(event) {
   }
 }
 
-buttonEl.addEventListener('click', onCreateNotification);
+// buttonEl.addEventListener('click', onCreateNotification);
